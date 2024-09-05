27c52b24ac17d221abcf2e632abdf12e4006860f
Vikash <119168528+vikash0p@users.noreply.github.com>
2024-09-05 15:04:00 +0530

    change css in cart shopping page

--- components/cartComponet/CartShopping.tsx
+++ components/cartComponet/CartShopping.tsx
@@ -9,62 +9,75 @@ import { IoTrashBinSharp } from "react-icons/io5";
 import { toastSuccess } from "@/utils/React-toastify";
 import Link from "next/link";
 
-
 const CartShopping = () => {
   const { state, dispatch } = useGlobalCartContext();
 
   if (state.cart.length === 0) {
     return (
-      <div className="centerdiv text-2xl font-bold flex-col gap-8">
+      <div className="centerdiv text-xl md:text-2xl font-bold flex-col gap-8">
         <h5>Your cart is empty</h5>
-<Link href={"/collection"} className="px-8 py-2 bg-primary">Collection</Link>
+        <Link
+          href={"/collection"}
+          className="px-6 py-2 bg-primary text-white rounded"
+        >
+          Collection
+        </Link>
       </div>
     );
   }
 
   return (
     <section>
-      <div className="bg-gray-100 min-h-screen py-8">
+      <div className="bg-gray-100 min-h-screen py-6 md:py-8">
         <div className="container mx-auto px-4">
-          <h1 className="text-2xl font-semibold mb-4">Shopping Cart</h1>
-          <div className="flex flex-col md:flex-row gap-4">
-            <div className="md:w-3/4">
-              <div className="bg-white rounded-lg shadow-md p-6 mb-4 ">
+          <h1 className="text-xl md:text-2xl font-semibold mb-4">
+            Shopping Cart
+          </h1>
+          <div className="flex flex-col lg:flex-row gap-4">
+            {/* Cart Items Section */}
+            <div className="w-full lg:w-3/4">
+              <div className="bg-white rounded-lg shadow-md p-4 md:p-6 mb-4">
                 <table className="w-full">
-                  <thead>
-                    <tr>
-                      <th className="text-left font-semibold">Product</th>
-                      <th className="text-left font-semibold">Size</th>
-                      <th className="text-left font-semibold">Price</th>
-                      <th className="text-left font-semibold">Quantity</th>
-                      <th className="text-left font-semibold">Total</th>
-                      <th className="text-left font-semibold">Remove</th>
-                    </tr>
-                  </thead>
-                  <tbody>
+                  <tbody className="text-sm md:text-base">
                     {state.cart
                       .slice()
                       .reverse()
                       .map((item: addItemPayload) => (
-                        <tr key={item.id}>
-                          <td className="py-4">
-                            <div className="flex flex-col  gap-2">
+                        <tr
+                          key={item.id}
+                          className="border-b border-gray-300 flex flex-col md:flex-row md:justify-between md:items-center gap-4 py-4"
+                        >
+                          {/* Product Details */}
+                          <div className="flex w-full   items-center justify-between md:justify-around gap-8">
+                            <td className="flex flex-col  md:flex-row items-center gap-4">
                               <Image
                                 src={item.imageUrl}
                                 alt={item.name}
                                 width={64}
                                 height={64}
-                                className="h-16 w-16 mr-4 object-cover"
+                                className="w-16 h-16 object-cover rounded"
                               />
-                              <span className="font-semibold">{item.name}</span>
-                            </div>
-                          </td>
-                          <td className="py-4">{item.size} </td>
-                          <td className="py-4">${item.price.toFixed(2)}</td>
-                          <td className="py-4">
-                            <div className="flex items-center">
+                              <div>
+                                <span className="font-semibold block">
+                                  {item.name}
+                                </span>
+                                <span className="block text-gray-500">
+                                  Size: {item.size}
+                                </span>
+                              </div>
+                            </td>
+
+                            {/* Price */}
+                            <td className="text-center md:text-left py-2">
+                              ${item.price.toFixed(2)}
+                            </td>
+                          </div>
+
+                          {/* Quantity Control */}
+                          <div className="flex flex-row gap-8 justify-between md:justify-around items-center w-full">
+                            <td className="flex justify-center md:justify-start items-center gap-2">
                               <button
-                                className="border rounded-md py-2 px-4 mr-2 hover:border-primary hover:text-primary"
+                                className="border rounded-md p-2 hover:border-primary hover:text-primary"
                                 onClick={() =>
                                   dispatch({
                                     type: "DECREMENT_QTY",
@@ -74,11 +87,11 @@ const CartShopping = () => {
                               >
                                 <GrSubtract />
                               </button>
-                              <span className="text-center w-8">
+                              <span className="w-8 text-center">
                                 {item.quantity}
                               </span>
                               <button
-                                className="border rounded-md py-2 px-4 ml-2 hover:border-primary hover:text-primary transition-colors"
+                                className="border rounded-md p-2 hover:border-primary hover:text-primary"
                                 onClick={() =>
                                   dispatch({
                                     type: "INCREMENT_QTY",
@@ -88,34 +101,40 @@ const CartShopping = () => {
                               >
                                 <GrAdd />
                               </button>
-                            </div>
-                          </td>
-                          <td className="py-4">
-                            ${(item.price * item.quantity).toFixed(2)}
-                          </td>
-                          <td className="py-4">
-                            <button
-                              type="button"
-                              className="text-red-600"
-                              onClick={() => {
-                                dispatch({
-                                  type: "REMOVE_ITEM",
-                                  payload: item,
-                                });
-                                toastSuccess("successfully remove item");
-                              }}
-                            >
-                              <IoTrashBinSharp size={30} />
-                            </button>
-                          </td>
+                            </td>
+
+                            {/* Total Price */}
+                            <td className="text-center md:text-right py-2 hidden md:block">
+                              ${(item.price * item.quantity).toFixed(2)}
+                            </td>
+
+                            {/* Remove Item */}
+                            <td className="text-center md:text-right py-2">
+                              <button
+                                type="button"
+                                className="text-red-600 hover:text-red-700"
+                                onClick={() => {
+                                  dispatch({
+                                    type: "REMOVE_ITEM",
+                                    payload: item,
+                                  });
+                                  toastSuccess("Item removed successfully");
+                                }}
+                              >
+                                <IoTrashBinSharp size={24} />
+                              </button>
+                            </td>
+                          </div>
                         </tr>
                       ))}
                   </tbody>
                 </table>
               </div>
             </div>
-            <div className="md:w-1/4">
-              <div className="bg-white rounded-lg shadow-md p-6">
+
+            {/* Summary Section */}
+            <div className="w-full lg:w-1/4 md:w-96">
+              <div className="bg-white rounded-lg shadow-md p-4 md:p-6">
                 <h2 className="text-lg font-semibold mb-4">Summary</h2>
                 <div className="flex justify-between mb-2">
                   <span>Subtotal</span>
@@ -138,9 +157,9 @@ const CartShopping = () => {
                   <span>$0.00</span>
                 </div>
                 <hr className="my-2" />
-                <div className="flex justify-between mb-2">
-                  <span className="font-semibold">Total</span>
-                  <span className="font-semibold">
+                <div className="flex justify-between mb-2 font-semibold">
+                  <span>Total</span>
+                  <span>
                     $
                     {(
                       state.cart.reduce(
@@ -150,7 +169,7 @@ const CartShopping = () => {
                     ).toFixed(2)}
                   </span>
                 </div>
-                <button className="bg-primary  py-2 px-4 rounded-lg mt-4 w-full">
+                <button className="bg-primary text-white py-2 px-4 rounded-lg mt-4 w-full hover:bg-hoverColor transition-colors">
                   Checkout
                 </button>
               </div>
@@ -158,7 +177,8 @@ const CartShopping = () => {
           </div>
         </div>
       </div>
-      {/* similer jewllery product */}
+
+      {/* Similar Jewellery Section */}
       <div>
         <SimilerJewellery />
       </div>
